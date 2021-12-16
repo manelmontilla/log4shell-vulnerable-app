a115a56bfb4dba28df94a6cdf868c90a81e1e62e
Manel Montilla <manel.montilla@gmail.com>
2021-12-16 16:44:33 +0100

    Make vulnerable for all header values

--- src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
+++ src/main/java/fr/christophetd/log4shell/vulnerableapp/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,8 +16,10 @@ public class MainController {
     private static final Logger logger = LogManager.getLogger("HelloWorld");
 
     @GetMapping("/")
-    public String index(@RequestHeader("X-Api-Version") String apiVersion) {
-        logger.info("Received a request for API version " + apiVersion);
+    public String index(@RequestHeader Map<String, String> headers) {
+         headers.forEach((key, value) -> {
+             logger.info("Header: " + key + "Value: " + value);
+        });
         return "Hello, world!";
     }
 
